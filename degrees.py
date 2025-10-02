ed0cb6eea9ac6a48f946c59b7f7b16fe1be624bc
josnif <josephnifemi89@gmail.com>
2025-10-02 14:50:44 +0100

    add logic for shortest path

--- degrees.py
+++ degrees.py
@@ -91,9 +91,40 @@ def shortest_path(source, target):
 
     If no possible path, returns None.
     """
+    
+    # Initialize frontier to just the starting position
+    start = Node(state=source, parent=None, action=None)
+    frontier = StackFrontier()
+    frontier.add(start)
+    
+    # Initialize an empty explored set
+    explored = set()
+    
+    # Keep looping until solution found
+    while not frontier.empty():
+        # Choose a node from the frontier
+        node = frontier.remove()
+        
+        # If node is the goal, then we have a solution
+        if node.state == target:
+            path = []
+            while node.parent is not None:
+                path.append((node.action, node.state))
+                node = node.parent
+            path.reverse()
+            return path
 
-    # TODO
-    raise NotImplementedError
+        # Mark node as explored
+        explored.add(node.state)
+        
+        # Add neighbors to frontier
+        for movie_id, person_id in neighbors_for_person(node.state):
+            if not frontier.contains_state(person_id) and person_id not in explored:
+                child = Node(state=person_id, parent=node, action=movie_id)
+                frontier.add(child)
+                
+    # there is no possible path between two actors
+    return None
 
 
 def person_id_for_name(name):
